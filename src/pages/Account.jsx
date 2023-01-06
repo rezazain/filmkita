9466e86a6c8785c313cbc51bf59f230df0d3fa59
rezazain <fadillahreza09@gmail.com>
2023-01-06 18:52:35 +0700

    first commit

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Account = () => {
-  return (
-    <div>Account</div>
-  )
-}
+  return <div></div>;
+};
 
-export default Account
+export default Account;
